fae1a7e5610987ceb8217ac0e88103135896f688
Cristian Prieto <me@cprieto.com>
2015-03-25 14:07:13 +1100

    Added constants

---
+++ common/src/main/java/com/cprieto/octocity/common/PropertyNames.java
@@ -0,0 +1,13 @@
+package com.cprieto.octocity.common;
+
+public interface PropertyNames {
+    String PACKAGE_ID = "octocity.id";
+    String PACKAGE_TITLE = "octocity.title";
+    String PACKAGE_VERSION = "octocity.version";
+    String PACKAGE_AUTHORS = "octocity.authors";
+    String PACKAGE_OWNERS = "octocity.owners";
+    String PACKAGE_LICENSEURL = "octocity.license";
+    String PACKAGE_PROJECTURL = "octocity.project";
+    String PACKAGE_DESCRIPTION = "octocity.description";
+    String PACKAGE_NOTES = "octocity.notes";
+}
